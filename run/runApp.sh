2cd4b7e89522feeb87c2bd43859c6c89ba5c741c
Chad Hanley <cchanley2003@yahoo.com>
2016-06-30 15:23:12 -0600

    All output to null

--- run/runApp.sh
+++ run/runApp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -jar /var/clojure/clj_forms.jar > /dev/null &  echo $! > /tmp/my-app.pid
-exit
+java -jar /var/clojure/clj_forms.jar > /dev/null 2>&1 & echo $! > /tmp/my-app.pid
+
